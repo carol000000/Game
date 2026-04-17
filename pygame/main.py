a0365c3b261828412709a8b1bf72e23c58820112
gua <carol2008tw@gmail.com>
2026-04-17 12:02:09 +0800

    123

--- pygame/main.py
+++ pygame/main.py
@@ -1,15 +1,20 @@
 import pygame
 
+#設定
+FPS = 60
+
 #初始化.視窗
 pygame.init() #初始化
 
-screen = pygame.display.set_mode((1500,800))
-
+screen = pygame.display.set_mode((1500,800)) #視窗
+clock = pygame.time.Clock()
 running = True
 
 #遊戲迴圈
 while running:
-    #輸入
+    clock.tick(FPS) #一秒內最多執行n次FPS
+
+    #如果遊戲關閉
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -17,5 +22,7 @@ while running:
     #更新
 
     #畫面
+    screen.fill((96,152,44)) #視窗填滿顏色RGB
+    pygame.display.update() #更新畫面
 
 pygame.quit
